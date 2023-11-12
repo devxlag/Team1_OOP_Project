29b9da463f35bc578c980d2af80e425f22aba350
steviehsingh <stevieharrypersads@gmail.com>
2023-11-11 23:50:01 -0400

    Test cases

---
+++ src/main/java/team1project/LuggageManifest.java
@@ -0,0 +1,30 @@
+package team1project;
+import java.util.ArrayList;
+
+public class LuggageManifest {
+
+
+    public LuggageManifest() {
+
+    }
+
+    public ArrayList<LuggageSlip> getLuggageSlips() {
+        return null;
+    }
+
+    public String addLuggage(Passenger p, Flight f) {
+        return null;
+    }
+
+    private double getExcessLuggageCost(int numPieces, int numAllowedPieces) {
+        return 0.0;
+    }
+
+    public String getExcessLuggageCostByPassenger(String passportNumber) {
+        return "";
+    }
+
+    public String toString() {
+        return "";
+    }
+}
