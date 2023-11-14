4b4c273ad0ddc5d10e8fba7af98379af9114fb1e
Andy22468 <133901698+Andy22468@users.noreply.github.com>
2023-11-14 23:36:57 +0000

    Add files via upload
    just a minor problem with the display method in the JavaFile in the main. it is just printing an extra line which it is not supposed to.

---
+++ src/test/java/team1project/JavaFileTest.java
@@ -0,0 +1,71 @@
+package team1project;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Test;
+
+public class JavaFileTest {
+
+    @Test
+    public void testJavaFileCreation() {
+        JavaFile javaFile = new JavaFile("TestFile.java");
+        assertNotNull(javaFile);
+        assertEquals("TestFile.java", javaFile.getFileName());
+        assertNotNull(javaFile.getContents());
+        assertEquals("", javaFile.getContents());
+    }
+
+    @Test
+    public void testAppendContents() {
+        JavaFile javaFile = new JavaFile("TestFile.java");
+
+        javaFile.appendContents("public class Test {\n");
+        javaFile.appendContents("    public static void main(String[] args) {\n");
+        javaFile.appendContents("        System.out.println(\"Hello, World!\");\n");
+        javaFile.appendContents("    }\n");
+        javaFile.appendContents("}\n");
+
+        String expectedContents = "public class Test {\n" +
+                                  "    public static void main(String[] args) {\n" +
+                                  "        System.out.println(\"Hello, World!\");\n" +
+                                  "    }\n" +
+                                  "}\n";
+
+        assertEquals(expectedContents, javaFile.getContents());
+    }
+
+    @Test
+    public void testDisplay() {
+        JavaFile javaFile = new JavaFile("TestFile.java");
+
+        // Redirect standard out for testing
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        javaFile.appendContents("public class Test {\n");
+        javaFile.appendContents("    public static void main(String[] args) {\n");
+        javaFile.appendContents("        System.out.println(\"Hello, World!\");\n");
+        javaFile.appendContents("    }\n");
+        javaFile.appendContents("}\n");
+
+        javaFile.display();
+
+        String expectedOutput = "Java File: TestFile.java\n" +
+                                "Contents:\n" +
+                                "public class Test {\n" +
+                                "    public static void main(String[] args) {\n" +
+                                "        System.out.println(\"Hello, World!\");\n" +
+                                "    }\n" +
+                                "}\n";
+
+        assertEquals(expectedOutput, outputStream.toString());
+
+        
+        // Reset standard out
+        System.setOut(System.out);
+    }
+}
